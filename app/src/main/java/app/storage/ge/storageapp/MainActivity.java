276e3f6d40ed3d73870abd3c79b751901c9e7450
unknown <kortava_temo@mail.ru>
2019-01-15 02:05:07 +0400

    first commit

---
+++ app/src/main/java/app/storage/ge/storageapp/MainActivity.java
@@ -0,0 +1,64 @@
+package app.storage.ge.storageapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edit;
+    private TextView txtShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edit = (EditText) findViewById(R.id.editText);
+        txtShow = (TextView) findViewById(R.id.textView);
+    }
+
+    public void read(View view) {
+        try {
+            FileInputStream fileInput = openFileInput("example.txt");
+            InputStreamReader reader = new InputStreamReader(fileInput);
+            BufferedReader buffer = new BufferedReader(reader);
+            StringBuffer strBuffer = new StringBuffer();
+            String Lines;
+            while ((Lines = buffer.readLine()) != null) {
+                strBuffer.append(Lines + "\n");
+            }
+            txtShow.setText(strBuffer.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+
+
+}
+        public void write (View view) {
+       String myTxt = edit.getText().toString();
+        try {
+            FileOutputStream  fileOutput = openFileOutput("example.txt", MODE_PRIVATE);
+            fileOutput.write(myTxt.getBytes());
+            fileOutput.close();
+            edit.setText("");
+            Toast.makeText(MainActivity.this, "Text is Saved.", Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
